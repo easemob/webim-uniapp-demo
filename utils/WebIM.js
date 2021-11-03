c3681092cc2debc4fb1df216d89dc99d05aa1c1f
QWK-001 <QWK_001@163.com>
2021-11-03 14:29:55 +0800

    update sdk

--- utils/WebIM.js
+++ utils/WebIM.js
@@ -1,5 +1,5 @@
 // 小程序3.0sdk
-import websdk from "../newSDK/uni_sdk3.6.3";
+import websdk from "../newSDK/uni_sdk4.0.0";
 // import websdk from "../sdk3.0/src/wxEntry"
 // import websdk from '../newSDK/sdk/src/wxEntry.js';
 
