0b93760ef206e5f70af9694e2cb50a43d02ff0a1
myZhangDong <973849793@qq.com>
2022-05-16 09:58:34 +0800

    feat: update IM SDK 4.0.5

--- utils/WebIM.js
+++ utils/WebIM.js
@@ -1,5 +1,5 @@
 
-import websdk from "../newSDK/uni_sdk4.0.4";
+import websdk from "../newSDK/uni_sdk4.0.5";
 import config from "./WebIMConfig";
 console.group = console.group || {};
 console.groupEnd = console.groupEnd || {};
