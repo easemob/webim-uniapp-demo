94b23b632cfb00d2df21c2f81a15d1974a469546
lizg <903504686@qq.com>
2022-08-09 18:01:22 +0800

    fix: 会话推送免打扰

--- components/chat/pushStorage.js
+++ components/chat/pushStorage.js
@@ -1,21 +1,54 @@
 
-
-export default function pushStorageSave(data) {
-    let newAry = JSON.parse(uni.getStorageSync("pushStorageData")) || [];
-    console.log('newAry>>>', newAry);
+var WebIM = require("../../utils/WebIM")["default"];
+export function pushStorageSave(data) {
+    // JSON.parse(uni.getStorageSync("pushStorageData")) ||
+    let currentLoginUser = WebIM.conn.context.userId
+    let newObj = uni.getStorageSync("pushStorageData") || {};
+    let newAry = newObj[currentLoginUser] || [];
     const { userId, type } = data; 
     if (!newAry.includes(userId) && type === "add") {      
-        console.log('1>>>>>>');  
-        newAry.push(userId);
-    } else if (newAry.includes(userId) && type === "remove") {
-        console.log('2>>>>>');
+        newAry = newAry.concat(userId);
+    } else if (newAry.includes(userId)  && type === "remove") {
         newAry = newAry.filter(item => item !== userId);
     }
+    newObj[currentLoginUser] = newAry;
     uni.setStorage({
         key: 'pushStorageData',
-        data: JSON.stringify(newAry),
+        data: newObj,
         success: function (params) {
-            console.log('>>>>>><<<<<<<<', JSON.parse(uni.getStorageSync("pushStorageData")));
+            console.log('uni.setStorage>>>', uni.getStorageSync("pushStorageData"));
         }
     });
 };
+
+export function onGetSilentConfig(message) {
+    console.log('message>>>',message);
+    const currentLoginUser = WebIM.conn.context.userId;
+    const { from, to, type } = message;
+    const res = uni.getStorageInfoSync();
+    console.log("uni.getStorageSync('pushStorageData')>>>", uni.getStorageSync('pushStorageData'));
+    let pushObj = uni.getStorageSync('pushStorageData') || {};
+    console.log('obj>>>', pushObj);
+    let pushAry = pushObj[currentLoginUser] || [];
+    console.log('pushAry>>>', pushAry);
+    const option = {
+        conversationId: type === "chat" ? from : to,
+        type: type === "chat" ? "singleChat" : "groupChat",
+    };
+    console.log('option>>>',option);
+    WebIM.conn.getSilentModeForConversation(option).then((res) => {
+        if (res.data.type === "NONE") {
+            if (!pushAry.includes(option.conversationId)) {
+                pushAry.push(option.conversationId)
+            }
+        }
+        pushObj.pushAry = pushAry;
+        uni.setStorage({
+            key: 'pushStorageData',
+            data: pushObj,
+            success: function (params) {
+                console.log('>>>>>>', JSON.parse(uni.getStorageSync("pushStorageData")));
+            }
+        });
+    });
+}
