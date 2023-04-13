0b15ec677a36d17f2268a23ec2ca5cca364e6a1b
wangying <wangying@wangyingdeMacBook-Pro.local>
2023-04-13 15:49:58 +0800

    add uniappsdk4.1.5

--- utils/WebIM.js
+++ utils/WebIM.js
@@ -1,4 +1,4 @@
-import websdk from "../newSDK/Easemob-chat-4.1.4";
+import websdk from "../newSDK/Easemob-chat-4.1.5";
 import config from "./WebIMConfig";
 console.group = console.group || {};
 console.groupEnd = console.groupEnd || {};
