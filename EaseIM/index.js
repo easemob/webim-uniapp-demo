cc546bd1b10a3c2ecd804c3cc3f629960e67f24b
黄飞鹏 <1094521084@qq.com>
2023-08-01 13:53:47 +0800

    update:更新im sdk至4.2.0

--- EaseIM/index.js
+++ EaseIM/index.js
@@ -1,5 +1,4 @@
 import EaseSDK from 'easemob-websdk/uniApp/Easemob-chat';
-// import EaseSDK from 'easemob-websdk/uniApp/Easemob-chat-uniapp-log';
 import { EM_APP_KEY, EM_API_URL, EM_WEB_SOCKET_URL } from './config';
 let EMClient = (uni.EMClient = {});
 EMClient = new EaseSDK.connection({
@@ -8,5 +7,4 @@ EMClient = new EaseSDK.connection({
   url: EM_WEB_SOCKET_URL,
 });
 uni.EMClient = EMClient;
-EaseSDK.logger.enableAll();
 export { EaseSDK, EMClient };
