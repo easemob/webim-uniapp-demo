ee54817d4412b210b651b8623d899e9d12905023
黄飞鹏 <1094521084@qq.com>
2023-08-15 17:35:59 +0800

    modify:恢复登录方式为默认方式.

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -74,7 +74,7 @@ import { useLoginStore } from '@/stores/login';
 let times = 60;
 let timer;
 const loginState = reactive({
-  usePwdLogin: true, //是否用户名+密码方式登录
+  usePwdLogin: false, //是否用户名+密码方式登录
   name: '',
   psd: '',
   grant_type: 'password',
