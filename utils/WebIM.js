204c44083bc32c62fb3c0f68a43b522592522143
stwang <464905943@qq.com>
2023-11-14 10:01:27 +0800

    4.3.0 sdk release

--- utils/WebIM.js
+++ utils/WebIM.js
@@ -1,4 +1,4 @@
-import websdk from "../newSDK/Easemob-chat-4.2.1";
+import websdk from "../newSDK/Easemob-chat-4.3.0.js";
 import config from "./WebIMConfig";
 console.group = console.group || {};
 console.groupEnd = console.groupEnd || {};
