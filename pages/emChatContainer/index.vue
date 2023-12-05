26e4f0acdf28b35b6739b271ce8aa0be442f7bbe
黄飞鹏 <1094521084@qq.com>
2023-12-05 09:59:56 +0800

    del:踢出zpaging调整引入方式.

--- pages/emChatContainer/index.vue
+++ pages/emChatContainer/index.vue
@@ -14,6 +14,8 @@ export default {
     return {
       targetId: '',
       chatType: '',
+      //v-model绑定的这个变量不要在分页请求结束中自己赋值！！！
+      dataList: [],
     };
   },
   components: {
@@ -25,6 +27,14 @@ export default {
     this.targetId = option.targetId;
     this.chatType = option.chatType;
     this.$store.commit('SET_CHATING_USERID', option.targetId);
+  },
+  onPageScroll(e) {
+    //更新z-paging内部scrollTop
+    this.$refs.paging.updatePageScrollTop(e.scrollTop);
+    //如果滚动到顶部，触发加载更多聊天记录
+    if (e.scrollTop < 10) {
+      this.$refs.paging.doChatRecordLoadMore();
+    }
   },
   mounted() {
     // uni.setNavigationBarTitle({
@@ -81,6 +91,42 @@ export default {
         default:
           return null;
       }
+    },
+    queryList(pageNo, pageSize) {
+      //组件加载时会自动触发此方法，因此默认页面加载时会自动触发，无需手动调用
+      //这里的pageNo和pageSize会自动计算好，直接传给服务器即可
+      //模拟请求服务器获取分页数据，请替换成自己的网络请求
+      const params = {
+        pageNo: pageNo,
+        pageSize: pageSize,
+      };
+      this.$request
+        .queryChatList(params)
+        .then((res) => {
+          //将请求的结果数组传递给z-paging
+          this.$refs.paging.complete(res.data.list);
+        })
+        .catch((res) => {
+          //如果请求失败写this.$refs.paging.complete(false);
+          //注意，每次都需要在catch中写这句话很麻烦，z-paging提供了方案可以全局统一处理
+          //在底层的网络请求抛出异常时，写uni.$emit('z-paging-error-emit');即可
+          this.$refs.paging.complete(false);
+        });
+    },
+    doSend(msg) {
+      uni.showLoading({
+        title: '发送中...',
+      });
+      setTimeout(() => {
+        uni.hideLoading();
+        this.$refs.paging.addChatRecordData({
+          time: '',
+          icon: '/static/daxiong.jpg',
+          name: '大雄',
+          content: msg,
+          isMe: true,
+        });
+      }, 500);
     },
   },
   //   async onPullDownRefresh() {
