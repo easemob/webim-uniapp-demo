a025a49e45c720d3e5125c1fa30bcc564b00df02
黄飞鹏 <1094521084@qq.com>
2024-07-01 15:21:58 +0800

    upate:更新SDK至最新版本.开启uniapp平台上报能力.

--- EaseIM/index.js
+++ EaseIM/index.js
@@ -5,6 +5,7 @@ EMClient = new EaseSDK.connection({
   appKey: EM_APP_KEY,
   apiUrl: EM_API_URL,
   url: EM_WEB_SOCKET_URL,
+  enableReportLogs: true, //小程序平台（uniApp）开启日志上传。
 });
 uni.EMClient = EMClient;
 export { EaseSDK, EMClient };
